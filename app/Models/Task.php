374183c078ed8e6fe2f7df89d850544d750250d3
Mukesh Kumar <thappamkkumar@gmail.com>
2026-08-21 17:06:35 +0530

    Fix code style

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-     use HasFactory;
+    use HasFactory;
 
     protected $fillable = [
         'title',
